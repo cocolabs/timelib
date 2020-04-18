94e94ad8040cd85306861d22720874c2889862bc
Matthew Cain <yooksi@zoho.com>
2020-04-18 15:54:08 +0200

    Implement command feedback
    Resolves #6

--- src/main/java/io/yooksi/trcm/TickRateCommand.java
+++ src/main/java/io/yooksi/trcm/TickRateCommand.java
@@ -5,8 +5,12 @@ import com.mojang.brigadier.arguments.FloatArgumentType;
 
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
+import net.minecraft.util.text.StringTextComponent;
 
 public class TickRateCommand {
+
+	private static final String FEEDBACK_CHANGED = "Game tick rate changed from %.1f to %.1f";
+	private static final String FEEDBACK_NOT_CHANGED = "Game tick rate is already %.1f";
 
 	public static void register(CommandDispatcher<CommandSource> dispatcher) {
 
@@ -15,7 +19,18 @@ public class TickRateCommand {
 						.executes((c) -> setTickRate(c.getSource(), c.getArgument("rate", Float.class)))));
 	}
 
-	private static int setTickRate(CommandSource source, float rate) {
-		return (int) Tick.changeRate(rate);
+	private static int setTickRate(CommandSource source, float newRate) {
+
+		final float currentRate = Tick.getRate();
+		if (newRate != currentRate) {
+			String message = String.format(FEEDBACK_CHANGED, currentRate, newRate);
+			source.sendFeedback(new StringTextComponent(message), true);
+			return (int) Tick.changeRate(newRate);
+		}
+		else {
+			String message = String.format(FEEDBACK_NOT_CHANGED, currentRate);
+			source.sendFeedback(new StringTextComponent(message), true);
+			return (int) currentRate;
+		}
 	}
 }
