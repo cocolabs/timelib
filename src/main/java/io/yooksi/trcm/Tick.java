5d9bad47c708bc579b6e1e1d1bc321797335c7c7
Matthew Cain <yooksi@zoho.com>
2020-04-18 16:15:30 +0200

    Add default tick rate value

--- src/main/java/io/yooksi/trcm/Tick.java
+++ src/main/java/io/yooksi/trcm/Tick.java
@@ -2,18 +2,18 @@ package io.yooksi.trcm;
 
 public class Tick {
 
+	public static final float DEFAULT_RATE = 20.0f;
+
 	public static boolean process_entities = true;
-	private static float rate = 20.0f;
-	private static float mspt = 50.0f;
+	private static float rate = DEFAULT_RATE;
+	private static float mspt = (float)(1000.0 / rate);
 
 	public static float changeRate(float rate) {
 
 		Tick.rate = rate;
 		long mspt = (long) (1000.0 / Tick.rate);
-		if (mspt <= 0L)
-		{
-			mspt = 1L;
-			Tick.rate = 1000.0f;
+		if (mspt <= 0L) {
+			mspt = 1L; Tick.rate = 1000.0f;
 		}
 		Tick.mspt = (float) mspt;
 		return Tick.rate;
