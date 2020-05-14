6deb03a25e5899a23cf500cca2e3fafcd63a9c27
Matthew Cain <yooksi@zoho.com>
2020-05-14 19:33:39 +0200

    Apply license notice to files
    Resolves #24

--- src/main/java/io/yooksi/timelib/mixin/MinecraftServer_runMixin.java
+++ src/main/java/io/yooksi/timelib/mixin/MinecraftServer_runMixin.java
@@ -1,3 +1,16 @@
+/*
+ *  Copyright (C) 2020 Matthew Cain
+ *
+ *  This file is part of TimeLib.
+ *
+ *  TimeLib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TimeLib. If not, see <https://www.gnu.org/licenses/>.
+ */
 package io.yooksi.timelib.mixin;
 
 import java.util.function.BooleanSupplier;
